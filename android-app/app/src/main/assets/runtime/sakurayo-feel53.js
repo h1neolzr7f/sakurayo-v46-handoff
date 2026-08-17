03e5437048b68044afc87eca9d863b127e4dbdab
aitag再战一百年 <HufferMacrostie2996@hotmail.com>
2026-08-18 01:02:44 +0800

    Add feel53 behavior and 开发版 夜樱 Android shell.

---
+++ android-app/app/src/main/assets/runtime/sakurayo-feel53.js
@@ -0,0 +1,6 @@
+(function (global) {
+  "use strict";
+  if (global.SakurayoFeel53 && global.SakurayoFeel53.install) {
+    global.SakurayoFeel53.install();
+  }
+})(typeof window !== "undefined" ? window : globalThis);
