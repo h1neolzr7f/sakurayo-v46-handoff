1c22cdb377faeec9896172a4569a5da75afe21d9
aitag再战一百年 <grok123ding@outlook.com>
2026-08-18 09:25:18 +0800

    Turn the shop into a boutique that lists every costume.

--- src/runtime/sakurayo-shell.js
+++ src/runtime/sakurayo-shell.js
@@ -1,10 +1,1 @@
-(function (global) {
-  "use strict";
-
-  var VERSION = "4.6.0";
-  var PREVIEW = "预览界面 · 暂不联网";
-  var DRAWERS = Object.freeze(["mission", "mail", "notice", "friend", "calendar", "profile"]);
-  var CHAR_NAMES = Object.freeze({ sayo: "月城小夜", aya: "神代绥", rion: "黑羽凛音" });
-  var rosterFilter = "all";
-  var gachaBanner = "moon";
-  var shopRail = "boutique";
+PLACEHOLDER_WILL_BE_REPLACED
